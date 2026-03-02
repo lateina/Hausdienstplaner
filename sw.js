bda5792e177d07fce7f6ac2a7b055501d9f5298e
Stefan Wagner <stefan@pckl00019.uni-regensburg.de>
2026-03-02 08:04:11 +0100

    Fix background push by merging FCM into main sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,30 @@
-const CACHE_NAME = 'dienste-chat-v3';
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCKOZlEu5QaQ8ISjFmNFFp5AXqypjJ9VCc",
+  authDomain: "dienste-chat.firebaseapp.com",
+  projectId: "dienste-chat",
+  storageBucket: "dienste-chat.firebasestorage.app",
+  messagingSenderId: "25445990011",
+  appId: "1:25445990011:web:993bfdd9b93502653a6cde"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('[sw.js] Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: './icon_tight_192.png',
+    data: payload.data
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+const CACHE_NAME = 'dienste-chat-v4';
 const ASSETS = [
   './index.html',
   './manifest.json',
